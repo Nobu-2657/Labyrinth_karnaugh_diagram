cdb8825faaf83e05209908eff463f2c7f82e08b0
Nobu-2657 <221205009@ccmailg.meijo-u.ac.jp>
2024-05-08 09:59:56 +0900

    Revert "Merge branch 'title' into Warp"
    This reverts commit 2847eada24f0b64a30c73ad8ea4e9a38af597e40, reversing
    changes made to d43eac953663d7507bef20d09a4f05a56d3e3362.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,10 +11,14 @@ public class PlayerController : MonoBehaviour
     private float _horizontal;
     private float _vertical;
     private Vector3 _velocity;
-    private bool isJumping = false;
-
     private Vector3 _aim; 
     private Quaternion _playerRotation; 
+    private bool isJumping = false; // ジャンプ中かどうか
+    float walkingSpeed = 70.0f; //歩く速度
+    float runningSpeed = 100.0f; //走る速度
+    float rotationSpeed = 3.0f; //視点の回転速度
+    float normalJump = 4.0f; // 通常ジャンプの大きさ
+    float gravity = 10f; // 重力の大きさ
 
     void Start()
     {
@@ -29,42 +33,67 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKey("up")) 
-        {
-            _rigidbody.AddForce(transform.forward * 70.0f, ForceMode.Force);
+        //移動速度の設定、走るモーションの設定
+        float speed = walkingSpeed;
+        if (Input.GetKey("left shift")) {
+            speed = runningSpeed;
+            if (_rigidbody.velocity.magnitude > 0.1f){
+                _animator.SetBool("running", true);
+            }
+            else {
+                _animator.SetBool("running", false);
+            }
         }
-        if(Input.GetKey("down")) 
-        {
-            _rigidbody.AddForce(transform.forward * -70.0f, ForceMode.Force);
+        else {
+            _animator.SetBool("running", false);
         }
-        if(Input.GetKey("right")) 
-        {
-            transform.Rotate(0f, 3.0f, 0f);
-        }
-        if(Input.GetKey("left")) 
-        {
-            transform.Rotate(0f, -3.0f, 0f);
-        }
-
-        //ジャンプ処理
-        // if(Input.GetKey("space")&& !isJumping) 
-        // {
-        //     _rigidbody.AddForce(transform.up * 100.0f, ForceMode.Impulse);
-        //     isJumping = true;
-        // }
-
-        // void OnCollisionEnter(Collision collision)
-        // {
-        //     if(collision.gameObject.CompareTag("Floor"))
-        //     {
-        //         isJumping = false;
-        //     }
-        // }
-
+        
+        //歩くモーションの設定
         if (_rigidbody.velocity.magnitude > 0.1f) {
             _animator.SetBool("walking", true);
         } else {
             _animator.SetBool("walking", false);
+        }
+
+        //移動処理
+        if(Input.GetKey("up")) 
+        {
+            _rigidbody.AddForce(transform.forward * speed, ForceMode.Force);
+        }
+        if(Input.GetKey("down")) 
+        {
+            _rigidbody.AddForce(transform.forward * -7 / 10 * speed, ForceMode.Force); //後ろに下がる速度は前進速度の7割
+        }
+        if(Input.GetKey("right")) 
+        {
+            transform.Rotate(0f, rotationSpeed, 0f);
+        }
+        if(Input.GetKey("left")) 
+        {
+            transform.Rotate(0f, -1 * rotationSpeed, 0f);
+        }
+
+        //ジャンプ処理
+        if(Input.GetKey("space")&& !isJumping) 
+        {
+            _rigidbody.AddForce(transform.up * normalJump, ForceMode.Impulse);
+        }
+        Debug.Log(isJumping);
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+        // else {
+        //     //_rigidbody.AddForce(transform.up * -1 * gravity, ForceMode.Force);
+        // }
+    }
+    private void OnCollisionExit(Collision collision) {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
         }
     }
 }
