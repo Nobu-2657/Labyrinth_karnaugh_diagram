d05f96edd72b00ee7f80cd42a38ae20dccd9a98c
Nobu-2657 <221205009@ccmailg.meijo-u.ac.jp>
2024-05-22 11:59:13 +0900

    戻した

--- Assets/Scripts/OnTriggerStay1.cs
+++ Assets/Scripts/OnTriggerStay1.cs
@@ -1,28 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class OnTriggerStay1 : MonoBehaviour
 {
     [SerializeField] GameObject Panel1;
+    [SerializeField] GameObject KeyImage;
     [SerializeField] Transform Input1a;
     [SerializeField] Transform Input1b;
     [SerializeField] Transform Image1a;
     [SerializeField] Transform Image1b;
+    [SerializeField] Transform Vigenere;
 
     void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //Panel1.SetActive(true);
-
-            //Canvasの暗号表がnon-activeだったら...
-            Input1a.gameObject.SetActive(true);
-            Image1a.gameObject.SetActive(true);
-
-            //Canvasの暗号表がactiveだったら...
-            //Input1b.gameObject.SetActive(true);
-            //Image1b.gameObject.SetActive(true);
+            if (Vigenere.gameObject.activeSelf)
+            {
+                Input1b.gameObject.SetActive(true);
+                Image1b.gameObject.SetActive(true);
+            }
+            else
+            {
+                Input1a.gameObject.SetActive(true);
+                Image1a.gameObject.SetActive(true);
+            }
         }
     }
 
@@ -30,15 +34,16 @@ public class OnTriggerStay1 : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            //Panel1.SetActive(false);
-
-            //Canvasの暗号表がnon-activeだったら...
-            Input1a.gameObject.SetActive(false);
-            Image1a.gameObject.SetActive(false);
-
-            //Canvasの暗号表がactiveだったら...
-            //Input1b.gameObject.SetActive(false);
-            //Image1b.gameObject.SetActive(false);
+            if (Vigenere.gameObject.activeSelf)
+            {
+                Input1b.gameObject.SetActive(false);
+                Image1b.gameObject.SetActive(false);
+            }
+            else
+            {
+                Input1a.gameObject.SetActive(false);
+                Image1a.gameObject.SetActive(false);
+            }
         }
     }
 }
