a2ad4b60e05fb6a9ccfaeae19eb4bc0fda8875c9
yuto52 <221205013@ccmailg.meijo-u.ac.jp>
2024-05-24 14:13:49 +0900

    メッセージを追加しました

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+public class GameSystem : MonoBehaviour
+{
+    private int i = 0;
+    private int j = 0;
+    private int k = 0;
+    private int l = 0;
+    [SerializeField] Transform Vigenere;
+    public Fungus.Flowchart flowchart;
+    public string sendMessageAnotherRoute = "";
+    public string sendMessageNeedVigenere = "";
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EventLogObject"))
+        {
+            switch (other.gameObject.name)
+            {
+                case "EventLogObject1":
+                    if(i==0)
+                    {
+                        flowchart.SendFungusMessage(sendMessageAnotherRoute);
+                        i++;
+                    }
+                    break;
+
+                case "EventLogObject2":
+                    if (j == 0)
+                    {
+                        if (!Vigenere.gameObject.activeSelf)
+                        {
+                            flowchart.SendFungusMessage(sendMessageNeedVigenere);
+                            j++;
+                        }
+                    }
+                    break;
+
+                case "EventLogObject3":
+                    if (k == 0)
+                    {
+                        
+                        k++;
+                    }
+                    break;
+            }
+            
+        }
+    }
+
+}//end
