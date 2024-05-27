81fb062972cf5c1740217267061b8df8cbe7d71c
keita04 <221205011@ccmailg.meijo-u.ac.jp>
2024-05-27 15:32:03 +0900

    BGMの設定とタイトル変更

--- Assets/Scripts/AudioConfig.cs
+++ Assets/Scripts/AudioConfig.cs
@@ -7,32 +7,32 @@ using UnityEngine.Audio;
 public class AudioConfig : MonoBehaviour
 {
     [SerializeField] AudioMixer audioMixer;
-    [SerializeField] AudioSource seAudioSource;
     [SerializeField] AudioSource bgmAudioSource;
-    [SerializeField] Slider seSlider;
     [SerializeField] Slider bgmSlider;
 
     private void Start()
     {
+        //何もスライダーを触らなかった場合
+        PlayerPrefs.SetFloat("BGMVolume", 0);
+        PlayerPrefs.Save();
+
         //スライダーを触ったら音量が変化する
         bgmSlider.onValueChanged.AddListener((value) => 
         { 
+
             value = Mathf.Clamp01(value);
 
             //変化するのは-80~0までの間
             float decibel = 20f * Mathf.Log10(value);
             decibel = Mathf.Clamp(decibel, -80f, 0f);
+
+            Debug.Log("decibel" + decibel) ;
+
+            //BGMの音量を保存
+            PlayerPrefs.SetFloat("BGMVolume", decibel);
+            PlayerPrefs.Save();
+
             audioMixer.SetFloat("BGM", decibel);
-        });
-        //スライダーを触ったら音量が変化する
-        seSlider.onValueChanged.AddListener((value) => 
-        { 
-            value = Mathf.Clamp01(value);
-
-            //変化するのは-80~0までの間
-            float decibel = 20f * Mathf.Log10(value);
-            decibel = Mathf.Clamp(decibel, -80f, 0f);
-            audioMixer.SetFloat("SE", decibel);
         });
 
     }
